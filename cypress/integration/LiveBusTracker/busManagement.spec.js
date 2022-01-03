cd0f0930503ca0f16dafe2dfd28c1834ddcefe39
Dan Hirst <danhirst123@gmail.com>
2022-01-03 02:29:33 +0000

    update ids for cypress on bus management

--- cypress/integration/LiveBusTracker/busManagement.spec.js
+++ cypress/integration/LiveBusTracker/busManagement.spec.js
@@ -20,7 +20,7 @@ describe('example to-do app', () => {
         // We use the `cy.get()` command to get all elements that match the selector.
         // Then, we use `should` to assert that there are two matched items,
         // which are the two default items.
-        cy.get('table').find('delete').last()
+        cy.get('table').find('#delete').last().click()
     })
 
 
