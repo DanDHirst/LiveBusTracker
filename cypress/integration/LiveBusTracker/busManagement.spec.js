2e78a4a471238e3839d098d3b839945a323e572c
Dan Hirst <danhirst123@gmail.com>
2022-01-04 00:59:48 +0000

    added manage route cypress intergration tests

--- cypress/integration/LiveBusTracker/busManagement.spec.js
+++ cypress/integration/LiveBusTracker/busManagement.spec.js
@@ -1,15 +1,8 @@
 describe('Bus Management', () => {
     beforeEach(() => {
-        // Cypress starts out with a blank slate for each test
-        // so we must tell it to visit our website with the `cy.visit()` command.
-        // Since we want to visit the same URL at the start of all our tests,
-        // we include it in our beforeEach function so that it runs before each test
         cy.visit('https://live-bus-tracker.herokuapp.com/busmanagement')
     })
     it('addBus to bus page', () => {
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
         cy.get('#nextStop').clear().type("Pilgrims Mews")
         cy.get('#Lat').clear().type("50.378278")
         cy.get('#Lng').clear().type("-4.148529")
@@ -23,9 +16,6 @@ describe('Bus Management', () => {
 
     })
     it('edit bus on the page', () => {
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
         cy.get('tr').last().find('#editBus').last().click()
         cy.get('#nextStop').clear().type("Pilgrims")
         cy.get('#Lat').clear().type("50.378277")
@@ -40,9 +30,6 @@ describe('Bus Management', () => {
     })
 
     it('remove bus on the page', () => {
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
         cy.get('table').find('#delete').last().click()
     })
     
--- cypress/integration/LiveBusTracker/routeManagement.spec.js
+++ cypress/integration/LiveBusTracker/routeManagement.spec.js
@@ -1,23 +1,51 @@
-describe('Bus Management', () => {
+describe('Manage routes', () => {
+    before(() => {
+
+        cy.visit('https://live-bus-tracker.herokuapp.com/busmanagement')
+        cy.get('#nextStop').clear().type("Pilgrims Mews")
+        cy.get('#Lat').clear().type("50.378278")
+        cy.get('#Lng').clear().type("-4.148529")
+        cy.get('#busID').clear().type("1000")
+        cy.get('body > :nth-child(6) > button').click()
+        cy.reload()
+
+    })
+    after(() => {
+        cy.visit('https://live-bus-tracker.herokuapp.com/busmanagement')
+        cy.get('table').find('#delete').last().click()
+    })
+
+
     beforeEach(() => {
-        // Cypress starts out with a blank slate for each test
-        // so we must tell it to visit our website with the `cy.visit()` command.
-        // Since we want to visit the same URL at the start of all our tests,
-        // we include it in our beforeEach function so that it runs before each test
         cy.visit('https://live-bus-tracker.herokuapp.com/busmanagement')
         cy.get('tr').last().find('#manageRoute').last().click()
 
     })
     it('addRoute to bus', () => {
-        // We use the `cy.get()` command to get all elements that match the selector.
-        // Then, we use `should` to assert that there are two matched items,
-        // which are the two default items.
         cy.get('#nextStop').clear().type("Pilgrims Mews")
         cy.get('#Lat').clear().type("50.378278")
         cy.get('#Lng').clear().type("-4.148529")
         cy.get('#submitRoute').click()
         cy.reload()
-        
+        cy.get("tr").last().find('#locName').should("have.text", "Pilgrims Mews")
+        cy.get("tr").last().find('#lat').should("have.text", "50.378278")
+        cy.get("tr").last().find('#lng').should("have.text", "-4.148529")
+    })
+    it('editroute to bus', () => {
+        cy.get("tr").last().find("#editRoute").click()
+        cy.get('#locName').clear().type("Pilgrims")
+        cy.get('#lat').clear().type("50.378277")
+        cy.get('#lng').clear().type("-4.148528")
+        cy.get('#submitRoute').click()
+        cy.reload()
+        cy.get("tr").last().find('#locName').should("have.text", "Pilgrims")
+        cy.get("tr").last().find('#lat').should("have.text", "50.378277")
+        cy.get("tr").last().find('#lng').should("have.text", "-4.148528")
+    })
+    it('Delete route from bus', () => {
+        cy.get("tr").last().find("#delete").click()
+        cy.get('table').find('tr').should('have.length', 1)
+
 
 
     })
